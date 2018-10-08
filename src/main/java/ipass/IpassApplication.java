ced12c252e1aae920644ea5c8ee338ab9fa87d59
hzwangqh <hzwangqihui@gmail.com>
2018-10-08 20:49:24 +0800

    'init'.

---
+++ src/main/java/ipass/IpassApplication.java
@@ -0,0 +1,12 @@
+package ipass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpassApplication.class, args);
+	}
+}
