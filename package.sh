ac4df8dafb2833b977a8bf0772c101b5b056909f
hzwangqh <hzwangqihui@gmail.com>
2018-10-09 22:07:35 +0800

    update

--- package.sh
+++ package.sh
@@ -5,7 +5,11 @@ git pull
 REL_DIR=/root/ipass
 
 
-sh mvnw clean package
+sh mvnw clean 
+
+cp conf/* $REL_DIR/
+
+sh mvnw package
 
 cp target/ipass-0.0.1-SNAPSHOT.jar $REL_DIR/
 cp scripts/start.sh $REL_DIR/
