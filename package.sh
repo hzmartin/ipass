c588ec4c75370248cc1399f6d78f828f524423a8
hzwangqh <hzwangqihui@gmail.com>
2018-10-09 22:16:34 +0800

    update

--- package.sh
+++ package.sh
@@ -12,5 +12,6 @@ cp conf/logback.xml $REL_DIR/
 sh mvnw package
 
 cp target/ipass-0.0.1-SNAPSHOT.jar $REL_DIR/
-cp scripts/start.sh $REL_DIR/
 cp conf/application.properties $REL_DIR/
+cp scripts/start.sh $REL_DIR/
+chmod +x $REL_DIR/start.sh
