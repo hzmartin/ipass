a4d68c9d99df05b38efbd44a14e39df2a50e6cfa
hzwangqh <hzwangqihui@gmail.com>
2018-10-09 20:17:02 +0800

    update

--- docs/start.sh
+++ docs/start.sh
@@ -4,7 +4,7 @@
 #环境变量及程序执行参数
 #需要根据实际环境以及Java程序名称来修改这些参数
 ###################################
-JAVA_HOME=/root/jre1.8.0_181
+JAVA_HOME=/root/jdk1.8.0_181
 #Java程序所在的目录（classes的上一级目录）
 APP_HOME=`pwd`
 LOG_DIR_OPT="-DAPP_HOME=\"${APP_HOME}\""
