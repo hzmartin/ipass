96f30c72e53c045d9cf6288484d4f143eabccccd
hzwangqh <hzwangqihui@gmail.com>
2018-10-09 11:46:27 +0800

    update

--- src/main/java/ipass/IPassApplication.java
+++ src/main/java/ipass/IPassApplication.java
@@ -1,8 +1,17 @@
 package ipass;
 
+import javax.sql.DataSource;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.alibaba.druid.pool.DruidDataSource;
 
 @SpringBootApplication
 @MapperScan("ipass.mapper")
@@ -11,4 +20,25 @@ public class IPassApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(IPassApplication.class, args);
 	}
+	
+	@Bean  
+    @ConfigurationProperties(prefix = "spring.datasource") 
+    public DataSource druidDataSource() {  
+        return new DruidDataSource();  
+    }  
+	
+	@Bean(name = "iEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword("511511");
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
 }
--- src/main/java/ipass/controller/IPassController.java
+++ src/main/java/ipass/controller/IPassController.java
@@ -2,7 +2,9 @@ package ipass.controller;
 
 import java.util.List;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +15,25 @@ import ipass.service.IPassService;
 @RestController
 public class IPassController {
 
+	@Value("${jasypt.test}")
+	private String testJasypt;
+	
 	@Autowired
 	private IPassService iPassService;
+
+	@Autowired
+	private StringEncryptor iEncryptor;
+
+	@RequestMapping("/encrypt")
+	public String encrypt(String text) {
+		return iEncryptor.encrypt(text);
+	}
 	
+	@RequestMapping("/testJasypt")
+	public String testJasypt() {
+		return testJasypt;
+	}
+
 	@RequestMapping("/create")
 	public String create() {
 		iPassService.create();
@@ -26,16 +44,14 @@ public class IPassController {
 	public List<IPass> selectAll() {
 		return iPassService.selectAll();
 	}
-	
+
 	@RequestMapping("/selectByAppUid")
 	public List<IPass> selectByAppUid(@RequestParam String appuid) {
 		return iPassService.selectByAppUid(appuid);
 	}
 
 	@RequestMapping("/insert")
-	public int insert(@RequestParam String appuid, 
-			@RequestParam String password,
-			@RequestParam String keyword,
+	public int insert(@RequestParam String appuid, @RequestParam String password, @RequestParam String keyword,
 			@RequestParam(required = false) String remark) {
 		IPass o = new IPass();
 		o.setAppuid(appuid);
